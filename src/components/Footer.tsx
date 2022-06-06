86227b4b9270e94e0a49b8df3d380c7a1ffa2e7f
nikitaisay <nikita.isay4work@gmail.com>
2022-06-06 22:46:56 +0300

    added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Footer: FC = () => {
+  return (
+    <footer className="footer">
+      <p>Если ты из россии - иди нахуй. 🇺🇦🇺🇦🇺🇦</p>
+    </footer>
+  );
+};
+
+export default Footer;
